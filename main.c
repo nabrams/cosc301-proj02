2838b6e22b2e5acbbfc330519bb4301c43ac29b3
Natalie Abrams <nabrams94@gmail.com>
2015-10-08 17:29:30 -0400

    added some helper functions plus built in mode use

--- main.c
+++ main.c
@@ -55,21 +55,60 @@ int arr_len(char **arr){
     return len;
 }
 
-void execute_command(char *tokens2[]){
+//need to find where to place this, not sure where the mode should be placed
+//in between commands? only applicable to one command or all?
+
+int run_mode(*tokens2[]){
+    int len = arr_len(tokens2);
+    for (int j=0; j<len; j++){
+        if (strcmp(tokens2[j], "p") == 0 || strcmp(tokens2[j],"parallel") ==0){
+            return 1; //Parallel mode
+        }
+    }
+    return 0; //Sequential mode
+}
+
+//helper function to see if it knows how to check if function is built in
+//(dont think we need to keep this after testing)
+char * built_in_check(* tokens2[]){
+    int len= arr_len(tokens2);
+    for (int k=0; k<len; k++){
+        printf("this is what youre checking %s \n", tokens2[k]);
+        if(strcmp(tokens2[k],"exit") ==0){
+            return "Yes";
+        }
+    }
+    return "no";
+    }
+void execute_command(char *tokens2[], int mode){
     int toklen = arr_len(tokens2); 
+    int childrv =0;
     for (int i=0; i<toklen; i++){
-        if (*tokens2[i] == "exit"){
+        if (strcmp(tokens2[i],"exit") ==0){
+            //printf("got here\n");
+            //its not waiting until child processes are done..any ideas?
+            wait(&childrv);
             exit(EXIT_SUCCESS);
         }
+        else if (strcmp(tokens2[i],"mode") == 0){
+            if (mode == 0){
+                printf("The current mode is Sequential \n");
+            }
+            else{
+                printf("The current mode is Parallel\n");
+            }
+        }
+        else{
         pid_t p = fork();
         if(p>0){
-            int childrv=0;
+            childrv=0;
             int id = wait(&childrv);
             }
         if (p == 0){
             execv(tokens2[i], tokens2);
             }
         printf("child process finished\n");
+        }
     }
 
 }
@@ -130,19 +169,30 @@ int main(int argc, char **argv) {
     fflush(stdout);
 
     char *buffer[1024];
-    int mode = 0;
+    int mode = 0; //starts in sequential mode
     while(fgets(buffer,1024,stdin) != NULL){
         remove_comments(buffer);
-        char **tokens = tokenify(buffer, ";\n");
+        char **tokens = tokenify(buffer, ";");
         int toklen = arr_len(tokens);
-        for (int j=0; j < toklen; j++){
-            char **tokens2 = tokenify(tokens[j], " \t");
-            execute_command(tokens2);
-            print_tokens(tokens2);
+        //printf("token length %d\n", toklen);
+        for (int j=0; j < toklen-1; j++){
+            char **tokens2 = tokenify(tokens[j], " \t\n");
+            //not sure whether to keep mode check here or inside execute function?
+            //printf("is it built in?: %s\n", built_in_check(tokens2));
+            //if (strcmp(tokens2,"exit") ==0){
+              //  printf("got here\n");
+                //exit(EXIT_SUCCESS);
+           // }
+            //else if (strcmp(tokens2,"mode") == 0){
+             //   printf("The current mode is %d\n", mode);
+            //}
+            mode = run_mode(tokens2); //get mode 
+            execute_command(tokens2,mode);
+            //print_tokens(tokens2);
             free_tokens(tokens2);
             //printf("%s\n", tokens[j]);
         }
-        print_tokens(tokens);
+        //print_tokens(tokens);
         free_tokens(tokens);
         //execute_command(buffer);
         printf("%s", prompt);
